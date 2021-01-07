14da683bd2ff1b2586a0be9299392b08bae18c84
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-01-07 00:00:59 -0500

    getting closer...

--- index.php
+++ index.php
@@ -1,65 +1,3 @@
-<?php
-
-// check to see if the server root is writable
-if (!is_writable('.')) {
-    $error .= 'Can\'t write to the current directory. Please fix this by giving the webserver user write access to the directory.<br/>';
-    echo ($error);
-}
-// check to see if the server root is readable
-if (!is_readable('.')) {
-    $error .= 'Can\'t read the current directory. Please fix this by giving the webserver user write access to the directory.<br/>';
-    echo ($error);
-}
-
-// Check the status of the installation from status.conf file which has 1 word in it
-global $status;
-$status = shell_exec('cat status.conf | head -n 1');
-echo $status;
-
-
-function checkDependants($status)
-{
-    // Check main java version to see if it is installed, should print out 'YES' if it is installed, and 'NO' if it is not
-    // POSIX:
-    $javaDependantCheck = shell_exec('command -v java >/dev/null && echo "YES" || echo "NO"');
-    if ($javaDependantCheck === "YES") {
-        runInstall($status);
-    }
-    // Windows:
-
-
-}
-
-function runInstall($status)
-{
-    if ($status == "installing") {
-        mkdir('server');
-        echo ("Made Server folder...");
-        $url = 'https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar';
-        $file_name = basename($url);
-        if (file_put_contents("server/$file_name", file_get_contents($url))) {
-            echo ("Downloaded server.jar to the current directory");
-        } else {
-            echo ("Downloading server.jar to the current directory failed. Please check the permissions of the index.php file located at " . $_SERVER['PHP_SELF']);
-        }
-    }
-}
-
-function getTitle($status)
-{
-    if ($status === "ready") {
-        $page_title = "Jovark Minecraft Web Installer Details";
-    } elseif ($status === "installing") {
-        $page_title = "Jovark Minecraft Web Installer";
-    } elseif ($status === "complete") {
-        $page_title = "Jovark Minecraft Management";
-    }
-    return $page_title;
-}
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -71,48 +9,97 @@ function getTitle($status)
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>
-        <?php
-        $title = getTitle($status);
-        ?>
+        Jovark Minecraft Web Installer
     </title>
 </head>
 
 <body>
+
     <?php
-    echo $page_title;
-    ?>
-    <!-- <form action="<?php $_SERVER['PHP_SELF']; ?>">
-        <p>
-            <label>Initial RAM:</label>
-            <input type="range" value="1024" min="512" max="3200" />
-        </p>
-        <p>
-            <label>Extra RAM:</label>
-            <input type="range" value="1024" min="0" max="3200" />
-        </p>
-        <p><input type="submit" /></p>
-    </form> -->
+    // check to see if the server root is writable
+    if (!is_writable('.')) {
+        $error .= 'Can\'t write to the current directory. Please fix this by giving the webserver user write access to the directory.<br/>';
+        echo ($error);
+    }
+    // check to see if the server root is readable
+    if (!is_readable('.')) {
+        $error .= 'Can\'t read the current directory. Please fix this by giving the webserver user write access to the directory.<br/>';
+        echo ($error);
+    }
+
+
+
+
+
+    // Check the status of the installation from status.conf file which has 1 word in it
+    $status = shell_exec('cat status.conf | head -n 1');
+    echo "Status: " . $status;
+    checkDependancies();
+
+
+    function checkDependancies()
+    {
+        echo "Checking deps";
+        // Check main java version to see if it is installed, should print out 'YES' if it is installed, and 'NO' if it is not
+        // POSIX
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            // if we are running Windows
+            $hasJava = shell_exec('java -version > NUL && echo yes || echo no');
+            if ($hasJava == "YES") {
+                echo "Running Installer...";
+                echo $hasJava;
+                runInstall();
+            } else {
+                echo "NO";
+                echo '<iframe src="javaHelper.html" height="500em" width="100%"></iframe>"';
+            }
+            echo "Windows";
+        } else {
+            // if we are running Linux, BSD, or macOS
+            $hasJava = shell_exec('command -v java >/dev/null && echo "YES" || echo "NO"');
+            if ($hasJava == 'YES') {
+                echo "Running Installer...";
+                echo $hasJava;
+                runInstall();
+            } elseif ($hasJava == 'NO') {
+                echo "NO";
+                echo '<iframe src="javaHelper.html" height="500em" width="100%"></iframe>"';
+            }
+            echo "Other";
+        }
+    }
+
+
+    function runInstall()
+    {
+        echo "Running installer actually...";
+        $CD = getcwd();
+        mkdir("$CD/server");
+        chdir("server");
+        echo ("Made Server folder...");
+        $url = 'https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar';
+        $file_name = basename($url);
+        if (file_put_contents($file_name, file_get_contents($url))) {
+            echo ("Downloaded server.jar to the current directory");
+        } else {
+            echo ("Downloading server.jar to the current directory failed. Please check the permissions of the index.php file located at " . $_SERVER['PHP_SELF']);
+        }
+        chdir("../");
+    }
+
+
+    // get title now so we can use it in the next bunch of HTML
+
+    echo ('
+
+
     <div>
         <div class="container">
             <div class="row">
-                <?php
-                $dependantStatus = checkDependants($status);
-                echo $dependantStatus;
-                echo ($error);
+            ');
+    //dynamic content here
+    echo ('
 
-                function displayJavaHelp($javaDependantCheck, $status)
-                {
-                    if (empty($javaDependantCheck)) {
-                        echo ("Something went wrong, and we cannot detect your version of Java.");
-                        echo ("<iframe src=\"javaHelper.html\" width=\"100%\" height=\"500em\" frameBorder=\"0\"></iframe>");
-                    } elseif ($javaDependantCheck == "version") {
-                        runInstall($status);
-                    }
-                }
-
-
-
-                ?>
             </div>
         </div>
     </div>
@@ -123,3 +110,5 @@ function getTitle($status)
 </body>
 
 </html>
+
+');
