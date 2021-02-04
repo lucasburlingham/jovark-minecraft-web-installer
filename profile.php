b77c3e289f1d9f962c9ce656fc79f05792f7edbc
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-02-04 12:58:00 -0500

    profile

--- profile.php
+++ profile.php
@@ -1,14 +1,14 @@
 <?php
 
 include("auth.php");
+require('db.php');
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-
     <meta charset="utf-8">
-    <title><?php echo $_SESSION['username']; ?>'s' Profile</title>
+    <title>Profile</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
@@ -26,18 +26,22 @@ include("auth.php");
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
                 <a class="nav-item nav-link active" href="#">User Profile</a>
-                <a class="nav-item nav-link" href="#">Features</a>
-                <a class="nav-item nav-link" href="#">Pricing</a>
-                <a class="nav-item nav-link" href="#">Disabled</a>
+                <a class="nav-item nav-link" href="index.php ">Home</a>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-success" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <?php echo $_SESSION['realname'];
+                    <a class="nav-link dropdown-toggle text-rimary float-right" href="#" id="navbarDropdown"
+                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <?php
+                        $username = $_SESSION['username'];
+                        $query = "SELECT realname from `users` WHERE username='$username'";
+                        $result = mysqli_query($con, $query) or die(mysqli_error($con));
+                        $row = mysqli_fetch_assoc($result);
+                        echo $row['realname'];
                         ?>
                     </a>
+
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Profile</a>
-                        <a class="dropdown-item" href="#">Prefrences</a>
+                        <a class="dropdown-item" href="profile.php">Profile</a>
+                        <a class="dropdown-item" href="#">Preferences</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="logout.php">Logout</a>
                     </div>
@@ -45,7 +49,8 @@ include("auth.php");
             </div>
         </div>
     </nav>
-
+    <?php
+    ?>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
