fff8a4741036e4035a70d5cddf95fe3f4b3d7823
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-02-04 21:21:16 -0500

    Ready for form (no action)

--- setup.php
+++ setup.php
@@ -62,6 +62,10 @@ require('db.php');
             <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
         </p>
     </div>
+
+    <!-- Insert form from test.html -->
+
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
